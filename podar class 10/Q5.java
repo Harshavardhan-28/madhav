4adaedfbf347a3a48822186a3e20f532da72c54f
Madhav singh <65648468+Madhav-s@users.noreply.github.com>
2020-06-07 23:00:55 +0530

    Update Q5.java

--- podar class 10/Q5.java
+++ podar class 10/Q5.java
@@ -1,18 +1,40 @@
- class Q5
+class patte2
 {
-    static void triangular_series(int n)
-    {
-        int i,j=1,k=1;
-        for(i=1;i<=n;i++){
-            System.out.printf("%d",k);
-            j=j+1;
-            k=k+j;
-        }
-    }
-    public static void main(String[]args)
-    {
-        int n = 5;
-        triangular_series(n);
-    }
+void printNums(char ch)
+{
+int n=0;
+switch (ch)
+{
+case'1':
+for(int i=5; i>n; i--)
+{
+int num=5;
+for(int j=0; j<=i; j++)
+{
+System.out.print(num+ " ");
+num--;
 }
-        
+
+
+System.out.println();
+}
+break;
+case'2':
+for (int k = 5; k >= 0; k--)
+{
+int alphabet = 65;
+for (int l = 0; l <= k; l++)
+{
+System.out.print((char) (alphabet + l) + " ");
+}
+System.out.println();
+}
+break;
+}
+}
+public static void main()
+{
+patte2 obj=new patte2();
+obj.printNums('1');
+}
+}
