568f6a7c005e771c25a9043d9726c4c705566379
Madhav singh <65648468+Madhav-s@users.noreply.github.com>
2020-06-07 23:07:18 +0530

    Update Q4.java

--- podar class 10/Q4.java
+++ podar class 10/Q4.java
@@ -1,33 +1,28 @@
-public class Q4
+class bank_deposit
 {
-    public static void main (String arg[]){
-        Scanner sc = new Scanner(System.in);
-        float P = 0;
-        double A = 0.0;
-        double r = 0.0;
-        int n= 0;
-        System.out.println("Main menu:-");
-        System.out.println("1-> Term Deposit");
-        System.out.println("2-> Recurring Deposit");
-        System.out.print("Enter your choice:");
-        int ch = sc.nextlnt();
-        switch(ch) {
-            case 1:
-            System.out.print("Enter pricipal amt:");
-            P=sc.nextDouble();
-            System.out.println("Enter time peiod in years: ");
-            n = sc.nextlnt();
-            A = P* Math.pow((1+r/100),n);
-            break;
-            case 2:
-            System.out.print("Enter time period in months: ");
-            n = sc.nextlnt();
-            A = P*n+P*n*(n+1)/2*r/100*1/12;
-            break;
-            default:
-            System.out.println("Invalid choice");
-        }
-        System.out.print("Maturity amount is: "+A);
-    }
+
+void switch_case(char a,double p,double r, double n)
+{
+double amt;
+switch(a)
+{
+case'1':
+amt=p*Math.pow((1+(r/100)),n);
+break;
+case'2':
+amt=(p*n)+(p*((n*n+n)/2)*(r/100)*(1/12));
+break;
+default:
+System.out.println("Invalid input");
+
 }
-            
+
+}
+
+public static void main()
+{
+bank_deposit obj=new bank_deposit();
+obj.switch_case('1',8000,9,12);
+}
+
+}
